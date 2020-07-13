fbbd10ae5ea16d227b1731f0601c63d4f231b218
Geovani Darcie <geovani_darcie@hotmail.com>
2020-07-13 20:21:48 -0300

    check-box

--- AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/Services/TodoItemService.cs
@@ -18,15 +18,14 @@ namespace AspNetCoreTodo.Services
         public async Task<TodoItem[]> GetIncompleteItemsAsync()
         {
             return await _context.Items
-            .Where(x => x.IsDone == false)
-            .ToArrayAsync();
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
         }
 
         public async Task<bool> AddItemAsync(TodoItem newItem)
         {
             newItem.Id = Guid.NewGuid();
             newItem.IsDone = false;
-            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
 
             _context.Items.Add(newItem);
 
@@ -34,5 +33,18 @@ namespace AspNetCoreTodo.Services
             return saveResult == 1;
         }
 
+        public async Task<bool> MarkDoneAsync(Guid id)
+        {
+            var item = await _context.Items
+                            .Where(x => x.Id == id)
+                            .SingleOrDefaultAsync();
+
+            if (item == null) return false;
+
+            item.IsDone = true;
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1; // One entity should have been updated
+        }
     }
 }
